15119e0f9487df8cca0440364d2ccd311ed3b9fb
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-07-13 03:24:37 +0500

    push

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,15 +1,25 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById('start-game-btn'); 
 
-const getPlayerChoice = function () {
-  const selection = prompt('Rock,Paper or Scissor?', '');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = function() {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
   if (
-    selection !== 'Rock' &&
-    selection !== 'Paper' &&
-    selection !== 'Scissors'
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
   ) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
   }
+  return selection;
 };
 
-startGameBtn.addEventListener('click', function () {
+startGameBtn.addEventListener('click', function() {
   console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
